fa3429b559040a45913bc31e6760751a93c34b49
RavenLB <sandra.poturalska@gmail.com>
2025-05-19 15:27:11 +0200

    feat(urls.py): Add URL patterns for log management endpoints

--- logger/urls.py
+++ logger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LogCreateView, LogListView
+from .views import LogCreateView, LogListView, LogExportView
 
 urlpatterns = [
-    path('logs/', LogCreateView.as_view(), name='log-create'),
-    path('logs/list/', LogListView.as_view(), name='log-list'),
+    path("logs/", LogCreateView.as_view(), name="log-create"),
+    path("logs/list/", LogListView.as_view(), name="log-list"),
+    path("logs/export/", LogExportView.as_view(), name="log-export"),
 ]
